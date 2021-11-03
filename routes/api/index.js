df5b49b2cda798a623f0fcb02bc250c7b158f70c
Yamil Chamo <yajochakha@gmail.com>
2021-11-03 15:10:55 -0500

    Fixing the server conection now to fix routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,11 +1,9 @@
 const router = require('express').Router();
-const apiRoutes = require('./users');
-const homeRoutes = require('./homeRoutes');
+const userRoutes = require('./userRoutes');
+const bookRoutes = require('./bookRoutes');
 
-const app = express();
 
-router.use('localhost:3000/books', homeRoutes);
-router.use('api', apiRoutes);
-
+router.use('/users', userRoutes);
+router.use('/books', bookRoutes);
 
 module.exports = router;
